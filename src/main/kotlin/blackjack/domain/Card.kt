cfe10f78d94c19c95b83a54aae5574239a8a4758
revfactory <revfactory@gmail.com>
2022-06-02 19:24:23 +0900

    feat: Modify card class to have grade

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -1,8 +1,9 @@
 package blackjack.domain
 
-data class Card(val suit: String) {
+data class Card(val suit: String = "클로버", val grade: String = "A") {
 
     companion object {
         val suits = arrayOf("클로버", "다이아", "스페이드", "하트")
+        val grades = arrayOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
     }
 }
--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -5,11 +5,29 @@ import io.kotest.matchers.shouldBe
 
 class CardTest : FunSpec({
     test("카드는 클로버, 다이아, 스페이드, 하트로 구성") {
-        Card.suits.map { Card(it) } shouldBe listOf(
-            Card("클로버"),
-            Card("다이아"),
-            Card("스페이드"),
-            Card("하트")
+        Card.suits.map { Card(suit = it) } shouldBe listOf(
+            Card(suit = "클로버"),
+            Card(suit = "다이아"),
+            Card(suit = "스페이드"),
+            Card(suit = "하트")
+        )
+    }
+
+    test("카드의 각 문양은 1(Ace) 부터 10까지 10장의 숫자 카드와 J(Jack), Q(Queen), K(King) 얼굴 카드로 구성") {
+        Card.grades.map { Card(grade = it) } shouldBe listOf(
+            Card(grade = "A"),
+            Card(grade = "2"),
+            Card(grade = "3"),
+            Card(grade = "4"),
+            Card(grade = "5"),
+            Card(grade = "6"),
+            Card(grade = "7"),
+            Card(grade = "8"),
+            Card(grade = "9"),
+            Card(grade = "10"),
+            Card(grade = "J"),
+            Card(grade = "Q"),
+            Card(grade = "K"),
         )
     }
 })
