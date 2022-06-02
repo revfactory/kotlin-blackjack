204336ee3f1bb83c462b491de7b1ee784e9c4735
revfactory <revfactory@gmail.com>
2022-06-02 19:54:22 +0900

    feat: Add point method in Card class

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -2,6 +2,14 @@ package blackjack.domain
 
 data class Card(val suit: String = "클로버", val grade: String = "A") {
 
+    fun point(): Int {
+        return when (grade) {
+            "A" -> listOf(1, 11).random()
+            "J", "Q", "K" -> 10
+            else -> grade.toInt()
+        }
+    }
+
     companion object {
         val suits = arrayOf("클로버", "다이아", "스페이드", "하트")
         val grades = arrayOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,6 +1,7 @@
 package blackjack.domain
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.shouldBe
 
 class CardTest : FunSpec({
@@ -29,5 +30,21 @@ class CardTest : FunSpec({
             Card(grade = "Q"),
             Card(grade = "K"),
         )
+    }
+
+    test("숫자 카드는 기본적으로 해당 숫자 만큼의 포인트로 계산하며, Ace 카드는 1 또는 11 포인트로, Face Card는 10 포인트로 계산") {
+        Card(grade = "A").point() shouldBeIn listOf(1, 11)
+        Card(grade = "2").point() shouldBe 2
+        Card(grade = "3").point() shouldBe 3
+        Card(grade = "4").point() shouldBe 4
+        Card(grade = "5").point() shouldBe 5
+        Card(grade = "6").point() shouldBe 6
+        Card(grade = "7").point() shouldBe 7
+        Card(grade = "8").point() shouldBe 8
+        Card(grade = "9").point() shouldBe 9
+        Card(grade = "10").point() shouldBe 10
+        Card(grade = "J").point() shouldBe 10
+        Card(grade = "Q").point() shouldBe 10
+        Card(grade = "K").point() shouldBe 10
     }
 })
